f9c701577e660a1121c4aa1999fc6791db3be900
Joris Bertier <joris.bertier@gmail.com>
2024-10-07 18:44:31 +0200

    create component Pokmeon spec & add image

---
+++ components/Pokemon/PokemonSpec.tsx
@@ -0,0 +1,16 @@
+import useThemeColors from "@/hooks/UseThemeColors"
+import { StyleSheet } from "react-native"
+
+type Props = {
+
+}
+export default function PokemonSpec() {
+    const colors = useThemeColors()
+    return(
+
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
